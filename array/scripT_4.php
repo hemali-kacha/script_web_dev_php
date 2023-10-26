8299f7b4524beeaa3b0f8c93367504c9bc8ec0da
hemali-kacha <143687370+hemali-kacha@users.noreply.github.com>
2023-10-26 14:43:16 +0530

    Check if the color "Green" exists in the array from question 1.

---
+++ array/scripT_4.php
@@ -0,0 +1,14 @@
+<?php
+	$color=array("pink","blue","violet","green","yellow");
+	
+if (in_array("green", $color))
+{
+	echo " green Color available";
+}
+else
+{
+	echo " green Color not available";
+}
+?>
+
+	
